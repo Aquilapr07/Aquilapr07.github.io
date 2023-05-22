c65ddc2632e1dce1b14180b125a6bbd37bc28087
Aquilapr07 <aquila.putra@mhs.itenas.ac.id>
2023-05-22 16:45:15 +0700

    commit awal

---
+++ application/views/front/partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="section p-3 bg-dark text-white">
+    <div class="text-center">&copy; IDA ROSILAWATI Copyright <?php echo date("Y"); ?> Aplikasi Pemesanan Online Cafe Doyan Mangan</div>
+</footer>
+</body>
+
+</html>
